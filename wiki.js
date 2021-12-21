36dcab92f9a4d3c8cfba7f4cceda153826d6f811
Jason Cordis <cordisjason@gmail.com>
2021-12-20 16:55:27 -0800

    commit

--- wiki.js
+++ wiki.js
@@ -1,4 +1,19 @@
 window.onload=function(){
+
+  if( navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+  ){
+     document.getElementById("mobile").innerHTML = "mobile"
+   }
+  else {
+    document.getElementById("mobile").innerHTML = "computer"
+   }
+
   var ele = document.getElementById("searchmovie");
  ele.innerHTML = '<br>'
  
